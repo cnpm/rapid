154e90adecf6ee8c00485e70e4b1ea66b07a9e35
fengmk2 <fengmk2@gmail.com>
2023-05-24 08:52:49 +0800

    deps: use urllib@3

--- packages/cli/lib/httpclient.js
+++ packages/cli/lib/httpclient.js
@@ -1,26 +1,9 @@
 'use strict';
 
-const urllib = require('urllib');
-const HttpAgent = require('agentkeepalive');
-const HttpsAgent = require('agentkeepalive').HttpsAgent;
+const { HttpClient } = require('urllib');
 const pkg = require('../package.json');
 
-const httpKeepaliveAgent = new HttpAgent({
-  keepAlive: true,
-  keepAliveMsecs: 30000,
-  maxSockets: 10,
-  maxFreeSockets: 10,
-});
-const httpsKeepaliveAgent = new HttpsAgent({
-  keepAlive: true,
-  keepAliveMsecs: 30000,
-  maxSockets: 100,
-  maxFreeSockets: 100,
-});
-
-const httpclient = new urllib.HttpClient2({
-  httpsAgent: httpsKeepaliveAgent,
-  agent: httpKeepaliveAgent,
+const httpclient = new HttpClient({
   defaultArgs: {
     headers: {
       referer: `${pkg.name}@${pkg.version}`,
